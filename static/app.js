d171207e9edd357f361fd356ff8fa287b2d85a91
Warly Martins Souza <warlymartinssouza760@gmail.com>
2025-06-03 01:59:34 -0300

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -3,3 +3,22 @@ function pesquisar() {
 
     alert(input_text);
 }
+
+
+
+const iconeFavorito = document.getElementById('favorito');
+const divFav = document.getElementById('div_fav');
+const textoStatus = document.getElementById('textoStatus');
+
+divFav.addEventListener('click', () => {
+  if (iconeFavorito.classList.contains('bi-star')) {
+    iconeFavorito.classList.remove('bi-star');
+    iconeFavorito.classList.add('bi-star-fill');
+    textoStatus.textContent = 'Favorito';
+  } else {
+    iconeFavorito.classList.remove('bi-star-fill');
+    iconeFavorito.classList.add('bi-star');
+    textoStatus.textContent = 'Favoritar';
+  }
+});
+
